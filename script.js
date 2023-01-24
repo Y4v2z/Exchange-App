d7b5365f58a7bde829d6f7ef1dba77d8d05d19bf
Yavuz Avci <yavuzavci01@gmail.com>
2023-01-24 16:19:50 +0200

    finished

--- script.js
+++ script.js
@@ -11,52 +11,41 @@ const inputs = document.querySelectorAll(".input");
 const currencyTypeOne = currencyOne.value;
 const currencyTypetwo = currencyTwo.value;
 const exchangeAmount = amount.value;
-inputs.forEach(input => {
-    input.addEventListener("keydown", handleEnterKey())
-})
-function handleEnterKey(e) {
-    let array = [currencyTypeOne, currencyTypetwo, exchangeAmount];
-    for (let value of array) {
-        if (e.key === "Enter") {
-            addEventCalculateBtn(value)
-        }
-    }
-};
-// Yada böyle tek tek yapmaya çalıştım ama ikisi de olmadı
-// function handleEnterKey(e) {
-//     const currencyTypetwo = currencyOne.value;
-//     if (e.key === "Enter") {
-//         addEventCalculateBtn(currencyTypetwo)
-//     }
-// }
-// function handleEnterKey(e) {
-//     const exchangeAmount = currencyOne.value;
-//     if (e.key === "Enter") {
-//         addEventCalculateBtn(exchangeAmount)
-//     }
-// };
-
-
-async function getCodes() {
-    try {
-        const response = await fetch(url + "/codes");
-        if (!response.result == "success") {
-            throw new Error("request was not successful")
-        }
-        const data = await response.json();
-        const items = await data.supported_codes;
-        let option;
-        for (let item of items) {
-            option += `<option value="${item[0]}">${item[1]}</option>`;
-        }
-        listOne.innerHTML = option;
-        listTwo.innerHTML = option;
-    }
-    catch (err) {
-    }
+function getCodes() {
+    fetch(url + "/codes")
+        .then(res => res.json())
+        .then(data => {
+            const items = data.supported_codes;
+            let option;
+            for (let item of items) {
+                option += `<option value="${item[0]}">${item[1]}</option>`
+            }
+            listOne.innerHTML = option;
+            listTwo.innerHTML = option;
+        })
 };
 getCodes();
-function addEventCalculateBtn() {
+// async function getCodes() {
+//     try {
+//         const response = await fetch(url + "/codes");
+//         if (!response.ok) {
+//             throw new Error("request was not successful")
+//         }
+//         const data = await response.json();
+//         const items = data.supported_codes;
+//         let option;
+//         for (let item of items) {
+//             option += `<option value="${item[0]}">${item[1]}</option>`;
+//         }
+//         listOne.innerHTML = option;
+//         listTwo.innerHTML = option;
+//     }
+//     catch (err) {
+
+//     }
+// }
+// getCodes();
+function exchange() {
     calculate.addEventListener("click", () => {
         const currencyTypeOne = currencyOne.value;
         const currencyTypetwo = currencyTwo.value;
@@ -64,6 +53,7 @@ function addEventCalculateBtn() {
         fetch(url + "/latest/" + currencyTypeOne)
             .then(res => res.json())
             .then(data => {
+                console.log(data);
                 const total = (data.conversion_rates[currencyTypetwo] * exchangeAmount).toFixed(3);
                 result.innerHTML = `
                 <div class="card border-primary">
@@ -77,36 +67,17 @@ function addEventCalculateBtn() {
                 `;
             })
     });
-}
-addEventCalculateBtn();
-// fetch(url + "/codes")
-//     .then(res => res.json())
-//     .then(data => {
-//         const items = data.supported_codes;
-//         let option;
-//         for (let item of items) {
-//             option += `<option value="${item[0]}">${item[1]}</option>`
-//         }
-//         listOne.innerHTML = option;
-//         listTwo.innerHTML = option;
-//     });
-// calculate.addEventListener("click", () => {
-//     const currencyTypeOne = currencyOne.value;
-//     const currencyTypetwo = currencyTwo.value;
-//     const exchangeAmount = amount.value;
-//     fetch(url + "/latest/" + currencyTypeOne)
-//         .then(res => res.json())
-//         .then(data => {
-//             const total = (data.conversion_rates[currencyTypetwo] * exchangeAmount).toFixed(3);
-//             result.innerHTML = `
-//             <div class="card border-primary">
-//                 <div class="card-body text-center fs-2">
-//                     ${exchangeAmount} ${currencyTypeOne} = ${total} ${currencyTypetwo}
-//                 </div>
-// <div class="card-footer text-muted text-center">
-//     1 ${currencyTypeOne}= ${data.conversion_rates[currencyTypetwo]} ${currencyTypetwo}
-// </div>
-//             </div>
-//             `;
-//         })
-// })
+};
+exchange();
+inputs.forEach(input => {
+    input.addEventListener("keydown", (e) => { handleEnterKey(e) })
+});
+function handleEnterKey(e) {
+    let inputsValue = [currencyTypeOne, currencyTypetwo, exchangeAmount];
+    for (let value of inputsValue) {
+        if (e.key === "Enter") {
+            exchange()
+        }
+    }
+};
+
